375934a564de469467697ff74e791a600dd58429
fadliadhitia <109494428+fadliadhitia@users.noreply.github.com>
2022-08-04 14:59:28 +0700

    Add files via upload

---
+++ Kelompok_IF6_Kepegawaian.sql
@@ -0,0 +1,159 @@
+-- MariaDB dump 10.19  Distrib 10.4.19-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: si_pegawai
+-- ------------------------------------------------------
+-- Server version	10.4.19-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `log_pegawai`
+--
+
+DROP TABLE IF EXISTS `log_pegawai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log_pegawai` (
+  `id_log` int(10) NOT NULL AUTO_INCREMENT,
+  `nip` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jabatan_lama` varchar(50) NOT NULL,
+  `jabatan_baru` varchar(50) NOT NULL,
+  `waktu` date NOT NULL,
+  PRIMARY KEY (`id_log`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `log_pegawai`
+--
+
+LOCK TABLES `log_pegawai` WRITE;
+/*!40000 ALTER TABLE `log_pegawai` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log_pegawai` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_pegawai`
+--
+
+DROP TABLE IF EXISTS `tb_pegawai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_pegawai` (
+  `nip` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `status` enum('Pegawai','Honorer') NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `foto` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_pegawai`
+--
+
+LOCK TABLES `tb_pegawai` WRITE;
+/*!40000 ALTER TABLE `tb_pegawai` DISABLE KEYS */;
+INSERT INTO `tb_pegawai` VALUES ('10120237','Fadli Adhitia Mahardika','Kp. Carik Kabupaten Bandung','085320433221','Pegawai','Manager','avatar.png');
+/*!40000 ALTER TABLE `tb_pegawai` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER update_jabatan 
+    BEFORE UPDATE 
+    ON tb_pegawai
+    FOR EACH ROW 
+BEGIN
+    INSERT INTO log_pegawai
+    set nip = OLD.nip,
+    nama = OLD.nama,
+    jabatan_lama=old.jabatan,
+    jabatan_baru=new.jabatan,
+    waktu = NOW(); 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `tb_pengguna`
+--
+
+DROP TABLE IF EXISTS `tb_pengguna`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_pengguna` (
+  `id_pengguna` int(11) NOT NULL,
+  `nama_pengguna` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `level` enum('Administrator','Sekretaris') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_pengguna`
+--
+
+LOCK TABLES `tb_pengguna` WRITE;
+/*!40000 ALTER TABLE `tb_pengguna` DISABLE KEYS */;
+INSERT INTO `tb_pengguna` VALUES (1,'Fadli Adhitia Mahardika','admin','admin','Administrator'),(2,'Sekertaris','sek','sek','Sekretaris');
+/*!40000 ALTER TABLE `tb_pengguna` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_profil`
+--
+
+DROP TABLE IF EXISTS `tb_profil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_profil` (
+  `id_profil` int(11) NOT NULL,
+  `nama_profil` varchar(50) CHARACTER SET utf16 NOT NULL,
+  `alamat` text CHARACTER SET latin1 NOT NULL,
+  `bidang` varchar(50) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_profil`
+--
+
+LOCK TABLES `tb_profil` WRITE;
+/*!40000 ALTER TABLE `tb_profil` DISABLE KEYS */;
+INSERT INTO `tb_profil` VALUES (1,'PT JAYA SELALU BERSAMA','BANDUNG','PROPERTI');
+/*!40000 ALTER TABLE `tb_profil` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-03 14:36:07
